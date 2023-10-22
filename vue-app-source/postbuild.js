f3eaa336fc79d1d66e5672654d615ec2a2e91b4b
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2023-10-22 16:37:30 +0300

    vue app making process; added postbuild.js

--- vue-app-source/postbuild.js
+++ vue-app-source/postbuild.js
@@ -1,22 +1,21 @@
 import fs from 'node:fs';
 
 
-const RE_head = /head/;
-const RE_assetUrl = /="()\/assets/;
+const RE_head = /<head>([\s\S]*)<\/head>/;
+const RE_assetUrl = /="\/assets/g;
 
 
-const linkCss = '<link rel="stylesheet" href="/style.css">';
-const assetUrlPathToAdd = '/vue-app';
+const add_linkCss = '<link rel="stylesheet" href="/style.css">';
+const add_assetUrlPath = '="/vue-app/assets';
 
 
 const html = fs.readFileSync('../vue-app/build/index.html', 'utf8');
 
+const re_HEAD = html.match(RE_head);
 
-let fixedHTML = '';
-let fixedHead = '';
+const headWithReplacedUrls = re_HEAD[1].replaceAll(RE_assetUrl, add_assetUrlPath)
+const headWithAddedCss = headWithReplacedUrls + add_linkCss;
 
-const re_resultHEAD = html.match(RE_head);
-
-
+const fixedHTML = html.replace(re_HEAD[1], headWithAddedCss);
 
 fs.writeFileSync('../vue-app/build/index.html',  fixedHTML);
